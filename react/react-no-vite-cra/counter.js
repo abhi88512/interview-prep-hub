9ea6e7eaa6f6cc6862a386f67bb3d93f4d670636
abhi88512 <abhilashmohanty2@gmail.com>
2025-09-20 17:35:45 +0530

    Implement counter functionality with increment and decrement buttons

--- react/react-no-vite-cra/counter.js
+++ react/react-no-vite-cra/counter.js
@@ -1,6 +1,15 @@
 const Counter = () => {
+    const [count, setCount] = React.useState(0);
 
-    return <div>Counter</div>;
+    return (
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount(count + 1)}
+                style={{ marginLeft: '8px' , color: 'white' , backgroundColor: 'green' }}> + </button>
+            <button onClick={() => setCount(count - 1)} 
+            style={{ marginLeft: '8px' , color: 'white' , backgroundColor: 'red' }}> - </button>
+        </div>
+    );
 }
 
 window.Counter = Counter;
